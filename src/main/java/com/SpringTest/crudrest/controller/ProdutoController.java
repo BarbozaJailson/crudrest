3fbe60b8ef6ba25e5f3d62e88a11ad047180693e
Jailson <jailson.100vicio@hotmail.com>
2025-03-21 22:47:13 -0300

    Adicionado produto controller

---
+++ src/main/java/com/SpringTest/crudrest/controller/ProdutoController.java
@@ -0,0 +1,46 @@
+package com.SpringTest.crudrest.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.SpringTest.crudrest.model.Produto;
+
+
+//curl POST http://localhost:8080/produtos -H "Content-Type: application/json; Charset=utf-8" -d @produto-pao.json
+
+@RestController
+public class ProdutoController {
+	
+	private static List<Produto> lista = new ArrayList<>();
+	
+	private static int proxId = 1;
+	
+	@GetMapping("/produtos")
+	public ResponseEntity<Iterable<Produto>> obterProdutos(){
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(lista);
+	}
+	
+	@PostMapping("/produtos")
+	public ResponseEntity<Produto> criarProduto(
+			@RequestBody Produto produto){
+		
+		produto.setId(proxId++);
+		
+		System.out.println(produto.toString());
+		lista.add(produto);
+		return ResponseEntity
+				.status(HttpStatus.CREATED)
+				.body(produto);
+				
+	}
+
+}
---
+++ src/main/java/com/SpringTest/crudrest/model/Produto.java
@@ -0,0 +1,85 @@
+package com.SpringTest.crudrest.model;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Produto {
+	
+	private Integer id;
+	private String nome;
+	private String descricao;
+	private BigDecimal preco; 
+	
+	public Produto(Integer id, String nome, String descricao, BigDecimal preco) {
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.preco = preco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(descricao, id, nome, preco);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(descricao, other.descricao) && Objects.equals(id, other.id)
+				&& Objects.equals(nome, other.nome) && Objects.equals(preco, other.preco);
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + ", nome=" + nome + ", descricao=" + descricao + ", preco=" + preco + "]";
+	}
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
